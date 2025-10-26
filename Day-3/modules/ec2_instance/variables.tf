d3c79d8460bccebede0b3046060c4ca700cbb474
ShaileshKalyane <Omprakashkalyane475@gmail.com>
2025-10-27 00:36:21 +0530

    Create variables.tf

---
+++ Day-3/modules/ec2_instance/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+    description = "value of ami"
+}
+variable "instance_type_value" {
+    description = "value of instance_type"
+}
+variable "subnet_value" {
+    description = "value of subnet"
+}
