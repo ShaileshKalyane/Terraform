a751bc3e0007e933541809559dbdddeace528545
ShaileshKalyane <Omprakashkalyane475@gmail.com>
2025-10-27 00:40:28 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+     region = "ap-south-1" 
+}
 
+ module "ec2_instance" {
+   source = "./modules/ec2_instance"
+   ami_value = "ami-0f918f7e67a3323f0"
+   instance_type_value = "t2.micro"
+   subnet_value = "subnet-0769134dc2df383dd"
+
+ }
