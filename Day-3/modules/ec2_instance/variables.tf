b11b129f261c284aea2af27b2f9b853a128f7cff
ShaileshKalyane <Omprakashkalyane475@gmail.com>
2026-02-21 20:53:40 +0530

    Delete Day-3 directory

--- Day-3/modules/ec2_instance/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_value" {
-    description = "value of ami"
-}
-variable "instance_type_value" {
-    description = "value of instance_type"
-}
-variable "subnet_value" {
-    description = "value of subnet"
-}
